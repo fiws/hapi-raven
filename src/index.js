5e2bac624d0158f3e30fbd8fb0ea5c47c521bfd4
Filip Weiss <me@fiws.net>
2016-09-27 15:31:04 +0200

    capture more errors by default

--- src/index.js
+++ src/index.js
@@ -5,27 +5,47 @@ var raven  = require('raven');
 exports.register = function (server, options, next) {
   var client = new raven.Client(options.dsn, options.client);
   server.expose('client', client);
-  server.on('request-error', function (request, err) {
-    var baseUrl = request.info.uri;
-    if (baseUrl === undefined) {
-      if (request.info.host) baseUrl = server.info.protocol + '://' + request.info.host;
-      else baseUrl = server.info.uri;
+  server.on({
+    name: 'request-internal',
+    filter: {
+      all: true,
+      tags: [ 'internal', 'implementation', 'error' ]
     }
-    (client.captureException || client.captureError).call(client, err, {
-      request: {
-        method: request.method,
-        url: baseUrl + request.path,
-        query_string: request.query,
-        headers: request.headers,
-        cookies: request.state,
-      },
-      extra: {
-        timestamp: request.info.received,
-        id: request.id,
-        remoteAddress: request.info.remoteAddress,
-      },
-      tags: options.tags
-    });
+  }, function (request, event) {
+    const err = event.data;
+    const meta = {
+      tags: options.events
+    };
+
+    if (!request) {
+      meta.extra = {
+        timestamp: event.timestamp,
+        logTags: event.tags
+      }
+      client.captureException(err, meta);
+    } else {
+      var baseUrl = request.info.uri;
+      if (baseUrl === undefined) {
+        if (request.info.host) baseUrl = server.info.protocol + '://' + request.info.host;
+        else baseUrl = server.info.uri;
+      }
+      (client.captureException || client.captureError).call(client, err, {
+        request: {
+          method: request.method,
+          url: baseUrl + request.path,
+          query_string: request.query,
+          headers: request.headers,
+          cookies: request.state,
+        },
+        extra: {
+          timestamp: request.info.received,
+          id: request.id,
+          remoteAddress: request.info.remoteAddress,
+          logTags: event.tags
+        },
+        tags: options.tags
+      });
+    }
   });
 
   next();
